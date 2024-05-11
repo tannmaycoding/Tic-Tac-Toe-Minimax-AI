731cf6de89cdc4ec0bb2379b03eb42c250ca9484
Tannmay Khandelwal <94522751+tannmaycoding@users.noreply.github.com>
2024-05-11 17:58:17 +0530

    Update tic tac toe.py

--- tic tac toe.py
+++ tic tac toe.py
@@ -1,17 +1,5 @@
 """
 This is a tic tac toe AI using minimax algorithm
-Modules used:
-  math,
-  copy
-
-Functions Made:
-  minimax: the minimax algorithm,
-  evaluate: evaluates the board,
-  is_terminal_state: returns if the match is over,
-  get_possible_moves: gives all possible moves,
-  get_chance: tells whose chance it is,
-  is_winner: tells if the player has won,
-  main: implementation of minimax algorithm
 """
 
 import math
